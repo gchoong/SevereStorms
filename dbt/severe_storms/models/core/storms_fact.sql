aad566c2fba8a6d1004c27847abc1166f074e0f4
gchoong <108086222+gchoong@users.noreply.github.com>
2025-04-13 22:13:15 -0500

    adding in clustering and partitioning

--- dbt/severe_storms/models/core/storms_fact.sql
+++ dbt/severe_storms/models/core/storms_fact.sql
@@ -1,26 +1,36 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    partition_by={
+        'field': 'event_begin_time',
+        'data_type': 'timestamp'
+    },
+    cluster_by=['event_type', 'state', 'event_year']
+) }}
 
-with main as (
-SELECT
-  event_id,
-  UPPER(state) as state,
-  SAFE_CAST(state_fips_code AS INT64) AS state_fips,
-  INITCAP(event_type) AS event_type,
-  event_begin_time,
-  event_end_time,
-  EXTRACT(YEAR FROM event_begin_time) AS event_year,
-  EXTRACT(MONTH FROM event_begin_time) AS event_month,
-  injuries_direct + injuries_indirect AS total_injuries,
-  deaths_direct + deaths_indirect AS total_deaths,
-  damage_property,
-  damage_crops,
-  damage_property + damage_crops AS total_damage,
-  source,
-  magnitude,
-  magnitude_type,
-  tor_f_scale,
-  CAST(event_latitude AS FLOAT64) AS lat,
-  CAST(event_longitude AS FLOAT64) AS lon
-FROM {{ ref('stg_storms_fact') }}
+WITH main AS (
+  SELECT
+    event_id,
+    UPPER(state) AS state,
+    SAFE_CAST(state_fips_code AS INT64) AS state_fips,
+    INITCAP(event_type) AS event_type,
+    event_begin_time,
+    event_end_time,
+    EXTRACT(YEAR FROM event_begin_time) AS event_year,
+    EXTRACT(MONTH FROM event_begin_time) AS event_month,
+    injuries_direct + injuries_indirect AS total_injuries,
+    deaths_direct + deaths_indirect AS total_deaths,
+    damage_property,
+    damage_crops,
+    damage_property + damage_crops AS total_damage,
+    source,
+    magnitude,
+    magnitude_type,
+    tor_f_scale,
+    CAST(event_latitude AS FLOAT64) AS lat,
+    CAST(event_longitude AS FLOAT64) AS lon
+  FROM {{ ref('stg_storms_fact') }}
 )
-select * from main order by total_deaths desc
+
+SELECT *
+FROM main
+ORDER BY total_deaths DESC
